* Fixed frequency convertion bugs:   - Added factor of 2pi   - Took carrier into account.
* Added disclaimer.

* renamed start_time to start_sample.

qiskit/providers/aer/pulse_new/converters/pulse_to_signals.py
@@ -1,3 +1,19 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Pulse schedule to Signals converter.
+"""
+
 import numpy as np
 from typing import List
 
@@ -43,7 +59,7 @@ class InstructionToSignals:
             frequency_shifts[ch.name] = 0.
             signals[ch.name] = PiecewiseConstant(samples=[], dt=self._dt, name=ch.name, carrier_freq=carrier_freq)
 
-        for start_time, inst in schedule.instructions:
+        for start_sample, inst in schedule.instructions:
             ch = inst.channel.name
             phi = phases[ch]
             freq = frequency_shifts[ch]
@@ -53,9 +69,9 @@ class InstructionToSignals:
                 start_idx = len(signals[ch].samples)
                 for idx, sample in enumerate(inst.pulse.get_waveform().samples):
                     t = self._dt * (idx + start_idx)
-                    samples.append(sample * np.exp(1.0j * freq * t + 1.0j * phi))
+                    samples.append(sample * np.exp(2.0j * np.pi * freq * t + 1.0j * phi))
 
-                signals[ch].add_samples(start_time, samples)
+                signals[ch].add_samples(start_sample, samples)
 
             if isinstance(inst, ShiftPhase):
                 phases[ch] += inst.phase
@@ -67,6 +83,6 @@ class InstructionToSignals:
                 phases[ch] = inst.phase
 
             if isinstance(inst, SetFrequency):
-                frequency_shifts[ch] = inst.frequency
+                frequency_shifts[ch] = inst.frequency - signals[ch].carrier_freq
 
         return list(signals.values())

test/terra/pulse_new/models/test_pulse_to_signals.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2018, 2019.
+# (C) Copyright IBM 2020.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -16,7 +16,7 @@ Tests to convert from pulse schedules to signals.
 import numpy as np
 
 from qiskit.pulse import (Schedule, DriveChannel, Play, Drag, ShiftFrequency,
-                          GaussianSquare, ShiftPhase, Gaussian)
+                          SetFrequency, GaussianSquare, ShiftPhase, Gaussian, Constant)
 from test.terra.common import QiskitAerTestCase
 
 from qiskit.providers.aer.pulse_new.converters import InstructionToSignals
@@ -76,3 +76,29 @@ class TestPulseToSignals(QiskitAerTestCase):
 
         self.assertEquals(signals[0].carrier_freq, 5.5e9)
         self.assertEquals(signals[0]._dt, 0.222)
+
+    def test_shift_frequency(self):
+        """Test that the frequency is properly taken into account."""
+
+        sched = Schedule()
+        sched += ShiftFrequency(1.0, DriveChannel(0))
+        sched += Play(Constant(duration=10, amp=1.0), DriveChannel(0))
+
+        converter = InstructionToSignals(dt=0.222, carriers=[5.0])
+        signals = converter.get_signals(sched)
+
+        for idx in range(10):
+            self.assertEquals(signals[0].samples[idx], np.exp(2.0j * idx * np.pi * 1.0 * 0.222))
+
+    def test_set_frequency(self):
+        """Test that SetFrequency is properly converted."""
+
+        sched = Schedule()
+        sched += SetFrequency(4.0, DriveChannel(0))
+        sched += Play(Constant(duration=10, amp=1.0), DriveChannel(0))
+
+        converter = InstructionToSignals(dt=0.222, carriers=[5.0])
+        signals = converter.get_signals(sched)
+
+        for idx in range(10):
+            self.assertEquals(signals[0].samples[idx], np.exp(2.0j * idx * np.pi * -1.0 * 0.222))